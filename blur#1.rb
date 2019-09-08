23e176d851fd81bdea8549c091e651297a18d1f3
Powai Leung <pwleung1489@gmail.com>
2019-09-08 14:59:10 +0000

    input blur#1

--- blur#1.rb
+++ blur#1.rb
@@ -1,1 +1,24 @@
-puts "hello"
+
+# define a class
+class Image
+  # this method build on a array contain 4 array with 4 items in each array.
+  # name blur 
+  def initialize(image)
+    @image = image
+  end 
+  # this method define image
+  def image
+  [[0,0,0,0], [0,1,0,0], [0,0,0,1], [0,0,0,0]]
+  end  
+  # need method in class to output image of array
+  def output_image
+    @image.each { |image| puts image.join() }
+  end
+  
+  #test
+  image = Image.new([
+  [0,0,0,0], [0,1,0,0], [0,0,0,1], [0,0,0,0]
+  ])
+  image.output_image
+
+  end
